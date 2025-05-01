0e806cd7d70d4783e71eec6bf2a61e8b7f6243b1
AlexXxey2005 <yupzvb@5qb2emy.ru>
2025-05-01 23:08:06 +0300

    Task_1

---
+++ VR24/Assets/Script.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public Animator anim_;
+
+    private void OnMouseDown()
+    {
+        anim_.SetTrigger("Start");
+    }
+}
